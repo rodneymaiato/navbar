00bf8f0b7053f9d60c54d31faece8ca2d5dd9985
rodneymaiato <rod.maiato@gmail.com>
2023-02-09 18:35:59 -0800

    Navbar was created successfully

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function menuToggler() {
-  const hideMenu = document.querySelectorAll(".menu-item");
-  for (let i = 0; i < hideMenu.length; i++) {
-    hideMenu[i].hidden = true;
-  }
-}
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
